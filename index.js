2b9214fa4e0b1536b0caaf4c2d55e0c3412cf553
Fayzullo07 <91960612+Fayzullo07@users.noreply.github.com>
2022-03-21 17:49:51 +0500

    npm i express-handlebars

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 const express = require("express");
-
 const app = express();
+const exphbs = require("express-handlebars");
+// const path = require("path");
+
+const hbs = exphbs.create({
+    defaultLayout: "main",
+    extname: "hbs"
+});
+
+app.engine('hbs', hbs.engine);
+app.set("view engine", "hbs");
+app.set("views", "views")
 
 app.get('/', (req, res) => {
-    res.send("hello")
+    res.render('index')
+});
+
+app.get("/about", (req, res) => {
+    res.render("about")
 })
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Server has been started on port ${PORT}...`)
