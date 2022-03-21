727f0c9b775ef6ed1acb9be248a78d49cfc71f00
Fayzullo07 <91960612+Fayzullo07@users.noreply.github.com>
2022-03-21 18:43:41 +0500

    Navbar

--- index.js
+++ index.js
@@ -12,12 +12,18 @@ app.engine('hbs', hbs.engine);
 app.set("view engine", "hbs");
 app.set("views", "views")
 
+app.use(express.static("public"))
+
 app.get('/', (req, res) => {
-    res.render('index')
+    res.render('index', {title: "Main Page", isHome: true})
 });
 
-app.get("/about", (req, res) => {
-    res.render("about")
+app.get("/notebooks", (req, res) => {
+    res.render("notebooks", {title: "Notebooks", isNotebooks: true})
+})
+
+app.get("/add", (req, res) => {
+    res.render("add", {title: "Add Notebook", isAdd: true})
 })
 
 const PORT = process.env.PORT || 5000;
