5fb32efef23ee4681d7c74990b70e32fad45eb43
Fayzullo07 <91960612+Fayzullo07@users.noreply.github.com>
2022-03-23 23:27:22 +0500

    Finally basket improved

--- public/app.js
+++ public/app.js
@@ -1,10 +1,12 @@
-const priceNotebook = document.querySelectorAll(".price");
-
-priceNotebook.forEach((item) => {
-  item.textContent = new Intl.NumberFormat("us-US", {
+const toCurrency = (price) => {
+  return new Intl.NumberFormat("us-US", {
     currency: "usd",
     style: "currency",
-  }).format(item.textContent);
+  }).format(price);
+};
+
+document.querySelectorAll(".price").forEach((c) => {
+  c.textContent = toCurrency(c.textContent);
 });
 
 const $card = document.querySelector("#card");
@@ -15,10 +17,55 @@ if ($card) {
 
       fetch("/card/remove/" + id, {
         method: "delete",
-      }).then((res) => res.json())
+      })
+        .then((res) => res.json())
         .then((card) => {
-            console.log(card);
-        })
+          if (card.notebooks.length) {
+            const dynamicHtml = card.notebooks
+              .map((c) => {
+                return `
+                    <tr>
+                        <td>${c.title}</td>
+                        <td>${c.count}</td>
+                        <td>
+                            <button class="btn btn-small js-remove" data-id="${c.id}">Delete</button>
+                        </td>
+                    </tr>
+                    `;
+              })
+              .join("");
+            $card.querySelector("tbody").innerHTML = dynamicHtml;
+            $card.querySelector(".price").textContent = toCurrency(card.price);
+          } else {
+            $card.innerHTML = `
+                <div class="mt-100">
+                <div class="row">
+                  <div class="col-md-12">
+                    <div class="card bascket">
+                      <div class="card-body cart">
+                        <div class="col-sm-12 empty-cart-cls text-center">
+                          <img
+                            src="https://i.imgur.com/dCdflKN.png"
+                            width="130"
+                            height="130"
+                            class="img-fluid mb-4 mr-3"
+                          />
+                          <h3><strong>Your Cart is Empty</strong></h3>
+                          <h4>Add something to make me happy :)</h4>
+                          <a
+                            href="/notebooks"
+                            class="btn btn-primary cart-btn-transform m-3"
+                            data-abc="true"
+                          >continue shopping</a>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div
+                `;
+          }
+        });
     }
   });
 }
