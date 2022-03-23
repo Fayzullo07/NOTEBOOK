fe957d3d4a3bc8952d81111cb274e1ad9f3e1da4
Fayzullo07 <91960612+Fayzullo07@users.noreply.github.com>
2022-03-23 20:26:17 +0500

    Buy Noutbook's database

--- models/card.js
+++ models/card.js
@@ -1,10 +1,45 @@
+const path = require("path");
+const fs = require("fs");
+
+const pathToDb = path.join(path.dirname(process.mainModule.filename), "data", "card.json");
 class Card {
-    add(notebook) {
-        console.log(notebook)
+    static async add(notebook) {
+        const card = await Card.fetch();
+
+        const idx = card.notebooks.findIndex((c) => c.id === notebook.id);
+        const candidate = card.notebooks[idx];
+
+        if(candidate) {
+            candidate.count++;
+            card.notebooks[idx] = candidate;
+        }else {
+            notebook.count = 1;
+            card.notebooks.push(notebook);
+        }
+
+        card.price += +notebook.price;
+
+        return new Promise((resolve, reject) => {
+            fs.writeFile(pathToDb, JSON.stringify(card), (err) => {
+                if(err) {
+                    reject(err);
+                }else {
+                    resolve();
+                }
+            })
+        })
     }
 
-    fetch() {
-
+    static async fetch() {
+        return new Promise((resolve, reject) => {
+            fs.readFile(pathToDb, "utf-8", (err, content) => {
+                if(err) {
+                    reject(err);
+                }else {
+                    resolve(JSON.parse(content));
+                }
+            })
+        })
     }
 }
 
