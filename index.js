faa38a71b3d2a4f496c9774229b1cf4b42de6ef4
Fayzullo07 <91960612+Fayzullo07@users.noreply.github.com>
2022-04-02 06:23:05 +0500

    improved project

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
+const flash = require("connect-flash");
 const mongoose = require("mongoose");
-const Handlebars = require('handlebars')
-const { allowInsecurePrototypeAccess } = require('@handlebars/allow-prototype-access')
+const Handlebars = require("handlebars");
+const {
+  allowInsecurePrototypeAccess,
+} = require("@handlebars/allow-prototype-access");
 const exphbs = require("express-handlebars");
 const session = require("express-session");
 const MongoStore = require("connect-mongodb-session")(session);
@@ -10,7 +13,7 @@ const homeRoutes = require("./routes/home");
 const notebooksRoutes = require("./routes/notebooks");
 const addRoutes = require("./routes/add");
 const cardRoutes = require("./routes/card");
-const ordersRouter = require("./routes/orders");
+const ordersRoutes = require("./routes/orders");
 const authRoutes = require("./routes/auth");
 const User = require("./models/user");
 const varMiddleware = require("./middleware/var");
@@ -19,28 +22,32 @@ const userMiddleware = require("./middleware/user");
 const MONGODB_URI = "mongodb+srv://fayzullo:F4995875f@cluster0.tpf56.mongodb.net/shop";
 
 const hbs = exphbs.create({
-    defaultLayout: "main",
-    extname: "hbs",
-    handlebars: allowInsecurePrototypeAccess(Handlebars)
+  defaultLayout: "main",
+  extname: "hbs",
+  helpers: require("./utils"),
+  handlebars: allowInsecurePrototypeAccess(Handlebars),
 });
 
 const store = new MongoStore({
-    collection: "sessions",
-    uri: MONGODB_URI,
+  collection: "sessions",
+  uri: MONGODB_URI,
 });
 
-app.engine('hbs', hbs.engine);
+app.engine("hbs", hbs.engine);
 app.set("view engine", "hbs");
 app.set("views", "views");
 
 app.use(express.static("public"));
-app.use(express.urlencoded({extended: true}));
-app.use(session({
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  session({
     secret: "secret val",
     resave: false,
     saveUninitialized: false,
     store,
-}));
+  })
+);
+app.use(flash());
 app.use(varMiddleware);
 app.use(userMiddleware);
 
@@ -48,30 +55,20 @@ app.use("/", homeRoutes);
 app.use("/notebooks", notebooksRoutes);
 app.use("/add", addRoutes);
 app.use("/card", cardRoutes);
-app.use("/orders", ordersRouter);
+app.use("/orders", ordersRoutes);
 app.use("/auth", authRoutes);
 
-
 async function start() {
-    try {
-        await mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
-        // const candidate = await User.findOne();
-        // if (!candidate) {
-        //     const user = new User({
-        //         email: "fayzulloh@gmail.com",
-        //         name: "Fayzulloh",
-        //         cart: { items: [] },
-        //     });
-        //     await user.save();
-        // }
-        const PORT = process.env.PORT || 5000;
-        
-        app.listen(PORT, () => {
-            console.log(`Server has been started on port ${PORT}...`)
-        });
-    } catch (e) {
-        console.log(e);
-    }
+  try {
+    await mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () => {
+      console.log(`Server has been started on port ${PORT}...`);
+    });
+  } catch (e) {
+    console.log(e);
+  }
 }
 
 start();
--- routes/auth.js
+++ routes/auth.js
@@ -1,67 +1,71 @@
- const bcrypt = require("bcryptjs");
-const {Router} = require("express");
- const User = require("../models/user"); 
- const router = Router();
+const { Router } = require("express");
+const bcrypt = require("bcryptjs");
+const User = require("../models/user");
+const router = Router();
 
+router.get("/login", async (req, res) => {
+  res.render("auth/login", {
+    title: "Register",
+    isLogin: true,
+    error: req.flash("error"),
+    loginError: req.flash("loginError"),
+  });
+});
 
- router.get("/login", async (req, res) => {
-     res.render("auth/login", {
-         title: "Register",
-         isLogin: true
-     });
- });
+router.get("/logout", async (req, res) => {
+  req.session.destroy(() => {
+    res.redirect("/auth/login#login");
+  });
+});
 
- router.get("/logout", async (req, res) => {
-     req.session.destroy(() => {
-        res.redirect("/auth/login#login");
-     });
- })
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const candidate = await User.findOne({ email });
 
- router.post("/login", async (req, res) => {
-    try {
-        const {email, password} = req.body;
-        const candidate = await User.findOne({email});
+    if (candidate) {
+      const samePas = bcrypt.compare(password, candidate.password);
+      if (samePas) {
+        req.session.user = candidate;
+        req.session.isAuthenticated = true;
+        req.session.save((err) => {
+          if (err) throw err;
 
-        if (candidate) {
-            const samePas = bcrypt.compare(password, candidate.password);
-            if (samePas) {
-                req.session.user = candidate;
-                req.session.isAuthenticated = true;
-                req.session.save(err => {
-                    if(err) throw err;
-                    res.redirect("/");
-                });
-            }
-        } else {
-            res.redirect("/auth/login#login");
-        }
-    } catch (e) {
-        console.log(e);
+          res.redirect("/");
+        });
+      }
+    } else {
+      req.flash("loginError", "Password wrong");
+      res.redirect("/auth/login#login");
     }
- });
+  } catch (e) {
+    req.flash("loginError", "This username does not found");
+    console.log(e);
+  }
+});
 
- router.post("/register", async (req, res) => {
-     try {
-        const {email, password, name} = req.body;
-        const candidate = await User.findOne({email});
+router.post("/register", async (req, res) => {
+  try {
+    const { email, password, name } = req.body;
+    const candidate = await User.findOne({ email });
 
-        if(candidate) {
-            res.redirect("/auth/login#register")
-        } else {
-            const hashPass = await bcrypt.hash(password, 10)
-            const user = new User({
-                email: email,
-                name: name,
-                password: hashPass,
-                cart: {items: []}
-            });
-            await user.save();
-            res.redirect("/auth/login#login")
-        }
-     } catch (e) {
-         console.log(e);
-     }
- })
+    if (candidate) {
+      req.flash("error", "This email is already exist");
+      res.redirect("/auth/login#register");
+    } else {
+      const hashPass = await bcrypt.hash(password, 10);
+      const user = new User({
+        email: email,
+        name: name,
+        password: hashPass,
+        cart: { items: [] },
+      });
+      await user.save();
+      res.redirect("/auth/login#login");
+    }
+  } catch (e) {
+    console.log(e);
+  }
+});
 
-
- module.exports = router;
+module.exports = router;
